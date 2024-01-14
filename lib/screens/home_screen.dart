26a7f029a121a0d16fe47981b0c662ce8af0adec
Anthonychibuike <Anthonychibuike246@gmail.com>
2024-01-14 15:31:42 +0100

    fix rerender

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:logger/logger.dart';
 import 'package:yahaal_test/blocs/recipe/recipe_bloc.dart';
 import 'package:yahaal_test/models/recipe.dart';
 import 'package:yahaal_test/screens/recipe_details.dart';
 import 'package:yahaal_test/screens/recipe_search_result.dart';
 import 'package:yahaal_test/utils/utilities.dart';
+
 import '../const.dart';
 import '../utils/app_colors.dart';
 import '../widgets/popular_card.dart';
 import '../widgets/primary_text.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -18,68 +18,71 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late RecipeBloc recipeBloc ;
+  late RecipeBloc recipeBloc;
   int selectedFoodCard = 0;
   Recipe recipeState = Recipe();
 
   @override
-  Widget build(BuildContext context) {
-    return Builder(
-      builder: (BuildContext context) {
-        recipeBloc = BlocProvider.of<RecipeBloc>(context)..add(FetchRecipesEvent());
-        return Scaffold(
-          backgroundColor: AppColors.white,
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            centerTitle: false,
-            elevation: 0,
-            title: const Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage('assets/man.jpeg'),
-              ),
-            ),
-            actions: [
-              IconButton(
-                onPressed: null,
-                icon: SvgPicture.asset('assets/menu.svg'),
-              ),
-              const SizedBox(
-                width: 20,
-              )
-            ],
-          ),
-          body: BlocConsumer<RecipeBloc, RecipeState>(
-            listener: (context,state){
-              if(state is RecipeSearchedState ){
-                Utilities.push(context, RecipeSearchResult(recipe: state.recipes,));
-              }
-            },
-  builder: (context, state) {
-    if(state is RecipeLoadingState){
-      return Utilities.showLoader();
-    }else if(state is RecipeLoadedState){
-        recipeState = state.recipes;
-        return  _buildRecipe(state.recipes);
-
-
-    }else if(recipeState.results != null){
-      return _buildRecipe(recipeState);
-
-
-    }else if(state is RecipeErrorState){
-      return Utilities.showError(state.errorMessage);
-    }
-    return Container();
-  },
-),
-        );
-      }
-    );
+  void initState() {
+    recipeBloc = BlocProvider.of<RecipeBloc>(context)..add(FetchRecipesEvent());
+    super.initState();
   }
 
-  Widget _buildRecipe(Recipe recipe){
+  @override
+  Widget build(BuildContext context) {
+    return Builder(builder: (BuildContext context) {
+      return Scaffold(
+        backgroundColor: AppColors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          centerTitle: false,
+          elevation: 0,
+          title: const Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: CircleAvatar(
+              radius: 25,
+              backgroundImage: AssetImage('assets/man.jpeg'),
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: null,
+              icon: SvgPicture.asset('assets/menu.svg'),
+            ),
+            const SizedBox(
+              width: 20,
+            )
+          ],
+        ),
+        body: BlocConsumer<RecipeBloc, RecipeState>(
+          listener: (context, state) {
+            if (state is RecipeSearchedState) {
+              Utilities.push(
+                  context,
+                  RecipeSearchResult(
+                    recipe: state.recipes,
+                  ));
+            }
+          },
+          builder: (context, state) {
+            if (state is RecipeLoadingState) {
+              return Utilities.showLoader();
+            } else if (state is RecipeLoadedState) {
+              recipeState = state.recipes;
+              return _buildRecipe(state.recipes);
+            } else if (recipeState.results != null) {
+              return _buildRecipe(recipeState);
+            } else if (state is RecipeErrorState) {
+              return Utilities.showError(state.errorMessage);
+            }
+            return Container();
+          },
+        ),
+      );
+    });
+  }
+
+  Widget _buildRecipe(Recipe recipe) {
     return ListView(
       children: [
         Padding(
@@ -116,25 +119,24 @@ class _HomeScreenState extends State<HomeScreen> {
                   const SizedBox(width: 10),
                   Expanded(
                       child: TextField(
-
-                        onSubmitted: (input){
-                          if(input.isNotEmpty){
-                            recipeBloc.add(FindRecipeByNameEvent(input));
-                          }else{
-                            Utilities.showToast('Please provide a recipe name');
-                          }
-                        },
-                        decoration: const InputDecoration(
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                  width: 2, color: AppColors.lighterGray)),
-                          hintText: 'Search for recipe by name eg Pasta',
-                          hintStyle: TextStyle(
-                              color: AppColors.lightGray,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500),
-                        ),
-                      )),
+                    onSubmitted: (input) {
+                      if (input.isNotEmpty) {
+                        recipeBloc.add(FindRecipeByNameEvent(input));
+                      } else {
+                        Utilities.showToast('Please provide a recipe name');
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 2, color: AppColors.lighterGray)),
+                      hintText: 'Search for recipe by name eg Pasta',
+                      hintStyle: TextStyle(
+                          color: AppColors.lightGray,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  )),
                   const SizedBox(width: 20),
                 ],
               ),
@@ -142,9 +144,7 @@ class _HomeScreenState extends State<HomeScreen> {
               const Padding(
                 padding: EdgeInsets.only(left: 20),
                 child: PrimaryText(
-                    text: 'Categories',
-                    fontWeight: FontWeight.w700,
-                    size: 22),
+                    text: 'Categories', fontWeight: FontWeight.w700, size: 22),
               ),
               SizedBox(
                 height: 240,
@@ -170,20 +170,18 @@ class _HomeScreenState extends State<HomeScreen> {
               Column(
                 children: List.generate(
                   recipe.results!.length,
-                      (index) {
+                  (index) {
                     Results recipeResult = recipe.results![index];
 
                     return GestureDetector(
-                      onTap: (){
+                      onTap: () {
                         Utilities.push(context, RecipeDetail(recipeResult));
                       },
                       child: PopularCard(
                         name: recipeResult.title,
                         star: "4.5",
                         weight: '10',
-
                         imagePath: recipeResult.image,
-
                       ),
                     );
                   },
@@ -196,12 +194,12 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-
   Widget foodCategoryCard(String imagePath, String name, int index) {
     return GestureDetector(
       onTap: () => {
         setState(
-              () => selectedFoodCard = index,),
+          () => selectedFoodCard = index,
+        ),
       },
       child: Container(
         margin: const EdgeInsets.only(right: 20, top: 20, bottom: 20),
@@ -209,7 +207,7 @@ class _HomeScreenState extends State<HomeScreen> {
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(20),
             color:
-            selectedFoodCard == index ? AppColors.primary : AppColors.white,
+                selectedFoodCard == index ? AppColors.primary : AppColors.white,
             boxShadow: const [
               BoxShadow(
                 color: AppColors.lighterGray,
